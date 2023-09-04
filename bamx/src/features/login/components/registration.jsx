45b129366539970a502ffd5105c9962212241fe7
fergomar1320 <09148@portalcedi.edu.mx>
2023-09-04 16:28:33 -0600

    Deleted MUI

--- bamx/src/features/login/components/registration.jsx
+++ bamx/src/features/login/components/registration.jsx
@@ -1,6 +1,5 @@
 import { useRef, useState, useEffect } from "react";
 import { RegistrationStyles } from "../styles/registrationStyles";
-import {Box} from '@mui/material'
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]),{8,24}$/;
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
 
@@ -48,10 +47,10 @@ const Registration = () => {
     }, [user, pwd, errMsg])
 
     return(
-        <Box style={RegistrationStyles.screen}>
-            <Box style={RegistrationStyles.logo}>
+        <Section style={RegistrationStyles.screen}>
+            <div style={RegistrationStyles.logo}>
                 <img width={'100%'} src = "https://s3-alpha-sig.figma.com/img/7949/0c4c/693a1740b876bde5ffe4761ed71a036b?Expires=1694390400&Signature=NxieEW4JVXhMt7Kr7OjYgiY9ut6Apy~~a0EXC-RtiVvyH5JUiDpLvMecYZFZLNJz6RAI2~2GvfvRnE3A2dEaum6s4sJwr8BZ~BzGV9s1BHZN1xoaFyY2RzhK47cz~UOL1pJ4gAbVUvd7gn9x0PKcwgJelVR487fCqDefILrXt4F-wTqdVARhbiWnCVaMo~FIKhl4F13ZmSqz0LWoKpoz0G8VOjBOP8U61I1tmdsicvbrzoUTbtAC7NFD-Ef8RRbB6ky5tTgmje8BZToD7lzDGT~4Cwj6Ou5DR15KkyhEPscli7b5CGmGt6HEkA0pQy05djzn8Eodt7axdkNbXlCJHg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="BAMX logo"/>
-            </Box>
+            </div>
             <p ref = {errRef} className = {errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
             <h1 className="loginText">¡Bienvenido!</h1>
             <form>
@@ -120,7 +119,7 @@ const Registration = () => {
                     Se admiten minus, mayus, numeros, y los caracteres '!@#$%' <br/>
                 </p>
             </form>
-        </Box>
+        </Section>
     )
 }
 
