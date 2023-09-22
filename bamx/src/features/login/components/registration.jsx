dd8aa7740fa154051332d074ffa6948299de3432
fergomar1320 <09148@portalcedi.edu.mx>
2023-09-22 14:10:44 -0600

    Fixed Styles

--- bamx/src/features/login/components/registration.jsx
+++ bamx/src/features/login/components/registration.jsx
@@ -34,7 +34,7 @@ const Registration = () => {
     return(
         <section className="screen">
             <div className="logo">
-                <img width={'100%'} src = "https://s3-alpha-sig.figma.com/img/7949/0c4c/693a1740b876bde5ffe4761ed71a036b?Expires=1694390400&Signature=NxieEW4JVXhMt7Kr7OjYgiY9ut6Apy~~a0EXC-RtiVvyH5JUiDpLvMecYZFZLNJz6RAI2~2GvfvRnE3A2dEaum6s4sJwr8BZ~BzGV9s1BHZN1xoaFyY2RzhK47cz~UOL1pJ4gAbVUvd7gn9x0PKcwgJelVR487fCqDefILrXt4F-wTqdVARhbiWnCVaMo~FIKhl4F13ZmSqz0LWoKpoz0G8VOjBOP8U61I1tmdsicvbrzoUTbtAC7NFD-Ef8RRbB6ky5tTgmje8BZToD7lzDGT~4Cwj6Ou5DR15KkyhEPscli7b5CGmGt6HEkA0pQy05djzn8Eodt7axdkNbXlCJHg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="BAMX logo"/>
+                <img width={'100%'} src = "https://firebasestorage.googleapis.com/v0/b/bamx-cc64f.appspot.com/o/Mobile%2Fassets%2Fsign_up%2Fbamx_logo.png?alt=media&token=bb2f494f-d3dc-41bc-87f7-b677e0e966d7" alt="BAMX logo"/>
             </div>
             <h1 className="loginText">¡Bienvenido!</h1>
             <form>
