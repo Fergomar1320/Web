2bacfdc969580c508f2024a058b639d7bb721a9f
Ingrid González <glez.ingrid23@gmail.com>
2023-10-17 12:31:33 -0600

    search bar and filter completed

--- bamx/src/features/dashboard/views/Dashboard.jsx
+++ bamx/src/features/dashboard/views/Dashboard.jsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
-import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import app from "../../../config/FirebaseConnection";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
-import { Navigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { db } from "../../../config/FirebaseConnection";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  query,
+  limit,
+} from "firebase/firestore";
 
-const auth = getAuth(app);  
-
+const auth = getAuth(app);
 
 const Dashboard = () => {
   const [pedidos, setPedidos] = useState([]);
-  const [user] = useAuthState(auth);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState(null); // Estado de filtrado
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -31,61 +35,126 @@ const Dashboard = () => {
   useEffect(() => {
     const getPedidos = async () => {
       try {
-        const userQuerySnapshot = await getDocs(collection(db, "userData"));
+        const userQuerySnapshot = collection(db, "userData");
         const docs = [];
-        userQuerySnapshot.forEach(async (doc) => {
-          try{
-            const requestsQuerySnapshot = await getDocs(collection(db, "userData", doc.id, "requestsHistory"));
-            console.log(requestsQuerySnapshot);
-            requestsQuerySnapshot.forEach((request) => {
-                docs.push({...request.data(), id: request.id, requestNumber: request.data()["requestID"], nameCorp: doc.data()["nameCorp"]});
-                setPedidos(docs);
+        const unsubscribe = onSnapshot(userQuerySnapshot, (snapshot) => {
+          snapshot.forEach((doc) => {
+            const requestsQuerySnapshot = query(
+              collection(db, "userData", doc.id, "requestsHistory"),
+              limit(50) // Limitar a 50 solicitudes
+            );
+            onSnapshot(requestsQuerySnapshot, (snapshot2) => {
+              snapshot2.docChanges().forEach((change) => {
+                if (change.type === "added") {
+                  docs.push({
+                    ...change.doc.data(),
+                    id: change.doc.id,
+                    nameCorp: doc.data()["nameCorp"],
+                  });
+                }
+              });
+              setPedidos(docs);
             });
-          } catch (e) {
-            console.log(e.message);
-          }
+          });
         });
+        return unsubscribe;
       } catch (error) {
         console.log("Error: ", error);
       }
     };
     getPedidos();
   }, []);
-  
-  if (!user) {
-    //redirect to login page
-    return <Navigate replace to = "/login" />
-  } else {
+
+  const filteredPedidos = pedidos.filter((pedido) =>
+    pedido.id.toLowerCase().includes(search.toLowerCase())
+  );
+
+  // Aplicar el filtro
+  const filteredPedidosByStatus = filter
+    ? filteredPedidos.filter((pedido) => pedido.status === filter)
+    : filteredPedidos;
+
   return (
     <section className="container">
       <section className="main">
-        <Header></Header>
+        <section className="header">
+          <section className="header-top">
+            <img
+              alt="bamx logo"
+              width="125"
+              height="102"
+              src={require("../../../global assets/favicon.png")}
+            />
+            <Form>
+              <InputGroup>
+                <Form.Control
+                  className="search"
+                  onChange={(e) => setSearch(e.target.value)}
+                  placeholder="Buscar por ID de pedido"
+                />
+              </InputGroup>
+            </Form>
+          </section>
+          <section className="header-bottom">
+            <section className="header-bottom_tittle">
+              <p> Solicitudes </p>
+            </section>
+            <section className="header-bottom_navbar">
+              <section className="header-bottom_buttons">
+                <button
+                  className={`btn1 ${filter === "Aceptado" ? "active" : ""}`}
+                  onClick={() => setFilter("Aceptado")}
+                >
+                  Aceptado
+                </button>
+                <button
+                  className={`btn2 ${filter === "Pendiente" ? "active" : ""}`}
+                  onClick={() => setFilter("Pendiente")}
+                >
+                  Pendientes
+                </button>
+                <button
+                  className={`btn3 ${filter === "Cancelado" ? "active" : ""}`}
+                  onClick={() => setFilter("Cancelado")}
+                >
+                  Cancelado
+                </button>
+                <button
+                  className={`btn4 ${filter === "En camino" ? "active" : ""}`}
+                  onClick={() => setFilter("En camino")}
+                >
+                  En camino
+                </button>
+              </section>
+            </section>
+          </section>
+        </section>
         <table className="styled-table">
           <thead>
-              <tr>
-                <th style={{ textAlign: "center" }}> Id</th>
-                <th style={{ textAlign: "center" }}> Nombre de la empresa </th>
-                <th style={{ textAlign: "center" }}> Estado del pedido </th>
-                <th style={{ textAlign: "center" }}> Fecha de creación </th>
-              </tr>
-            {pedidos.map((pedido) => (
+            <tr>
+              <th style={{ textAlign: "center" }}>Id del cargamento</th>
+              <th style={{ textAlign: "center" }}>Nombre de la empresa</th>
+              <th style={{ textAlign: "center" }}>Estado del pedido</th>
+              <th style={{ textAlign: "center" }}>Fecha de creación</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredPedidosByStatus.map((pedido) => (
               <tr className="data" key={pedido.id}>
-                <td style={{ textAlign: "center" }}>{`${pedido.requestNumber}` + pedido.id}</td>
+                <td style={{ textAlign: "center" }}>{`${pedido.id}`}</td>
                 <td style={{ textAlign: "center" }}>{pedido.nameCorp}</td>
                 <td style={{ textAlign: "center" }}>{pedido.status}</td>
                 <td style={{ textAlign: "center" }}>{pedido.creationDate}</td>
               </tr>
             ))}
-          </thead>
+          </tbody>
         </table>
       </section>
-
       <section className="side-bar">
         <Sidebar></Sidebar>
       </section>
     </section>
   );
-}
 };
 
 export default Dashboard;
